c42c98a9e5aa80d7749e2a2fa310ac1a576ec80d
Saskia Day <saskiaday@hotmail.com>
2023-08-27 12:24:06 +1000

    circles

--- week4.js
+++ week4.js
@@ -4,7 +4,15 @@
 let img; // Declare variable 'img'.
 
 function earth() {
-    circle(250,250,100);
+    
+    fill(0, 200, 0);
+    strokeWeight(8);
+    stroke('rgb(0,200,0)');
+    circle(250,250,100); 
+    fill(0, 0, 200);
+    arc(250, 250, 100, 100, HALF_PI + QUARTER_PI, PI + QUARTER_PI);
+    fill(0, 0, 200);
+    arc(250, 250, 100, 100, PI +HALF_PI + QUARTER_PI, TWO_PI + QUARTER_PI);
     
   }
 
@@ -16,8 +24,9 @@ function setup() {
   function draw() {
     background(220);
     
- 
+    stroke('rgb(0,0,0)');
     square(50,50, 400);
+    
     image(img, 50, 50, 400, 400);
     earth();
 
