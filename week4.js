9d44cd02ead70a0c66fecead0934a498cb0722ba
Saskia Day <saskiaday@hotmail.com>
2023-08-27 12:22:36 +1000

    yes

--- week4.js
+++ week4.js
@@ -1,3 +1,24 @@
 /*
     Paste the code for your week 4 exercise below.
 */
+let img; // Declare variable 'img'.
+
+function earth() {
+    circle(250,250,100);
+    
+  }
+
+function setup() {
+    createCanvas(500, 500);
+    img = loadImage('assets/fire.jpg'); // Load the image
+  }
+  
+  function draw() {
+    background(220);
+    
+ 
+    square(50,50, 400);
+    image(img, 50, 50, 400, 400);
+    earth();
+
+  }
