98488c0738e194f07d7d6d6cebd9000e9dbe3149
Saskia Day <saskiaday@hotmail.com>
2023-09-08 14:42:47 +1000

    WEEK6

--- week5.js
+++ week5.js
@@ -1,95 +1,67 @@
-/*
-    Paste the code for your week 5 exercise below.
-*/
-let img;
+// Where is the circle
+let x, y;
+let img; 
 
 // Writes the Logo Image
 function logoImage() {
+ 
+  square(50, 50, 400);
+  image(img, 50, 50, 400, 400);
 
-    square(50, 50, 400);
-    image(img, 50, 50, 400, 400);
 }
-
-// Writes the Logo Shape & Colour
-function logoShape() {
-
-    let lemonLogoCentreX;
-    let lemonLogoCentreY;
-    let lemonLogoHeight;
-
-    lemonLogoCentreX = 275;
-    lemonLogoCentreY = 250;
-    lemonLogoHeight = 200;
-
-    // Lemon Shape
-    strokeWeight(8);
-
-
-    for (let i = 0; i < 2; i++) {
-
-        let arcDiff = 50;
-        let lemonLogoWidthCalc = 200;
-        let arcMode;
-
-        if (i == 0) {
-            fill(246, 229, 20);
-            arcMode = CHORD;
-
-        } else {
-            fill(247, 235, 95);
-            lemonLogoWidthCalc = lemonLogoWidthCalc - arcDiff;
-        }
-        
-        arc(lemonLogoCentreX, lemonLogoCentreY, lemonLogoWidthCalc, lemonLogoHeight, HALF_PI, PI + HALF_PI, arcMode);
-
-    }
-    //   fill(246, 229, 20);
-    //   arc(lemonLogoCentreX, lemonLogoCentreY, 200, lemonLogoHeight, HALF_PI, PI + HALF_PI, CHORD);
-    //   fill(247, 235, 95);
-    //   arc(lemonLogoCentreX, lemonLogoCentreY, 150, lemonLogoHeight, HALF_PI, PI + HALF_PI);
-
-    // Lemon Lines
-    strokeWeight(3);
-
-    for (let i = 0; i < 3; i++) {
-        let lineDiff = 15;
-        let lemonLogoCentreXCalc = lemonLogoCentreX - 60;
-
-        if (i == 1) {
-            lemonLogoCentreXCalc = lemonLogoCentreXCalc - lineDiff;
-        }
-        line(lemonLogoCentreX, lemonLogoCentreY, lemonLogoCentreXCalc, lemonLogoCentreY - 64 + (64 * i));
-    }
-
-    //   line(lemonLogoCentreX, lemonLogoCentreY, lemonLogoCentreX - 60, lemonLogoCentreY + 64);
-    //   line(lemonLogoCentreX, lemonLogoCentreY, lemonLogoCentreX - 75, lemonLogoCentreY );
-    //   line(lemonLogoCentreX, lemonLogoCentreY, lemonLogoCentreX - 60, lemonLogoCentreY - 64);
-}
-
-// Writes the Logo Typography
 function logoTypography() {
 
     textWrap(CHAR);
     textStyle(BOLDITALIC);
     strokeWeight(0);
     textSize(50);
-    fill(0, 0, 0);
-    text('LUCYS', 170, 120,);
-    text('LEMONS', 150, 410);
-}
+    fill(0,0,0);
+    text('LUCYS', 170, 120, );
+    text('LEMONS', 150, 410 );
+  }
+
 
 function setup() {
-
-    createCanvas(500, 500);
-    img = loadImage('assets/backgroundlemon.jpg');
-
+  createCanvas(500, 500);
+  img = loadImage('assets/backgroundlemon.jpg');
+  // Starts in the middle
+  x = width / 2;
+  y = height;
 }
 
 function draw() {
-
-    background(250, 150, 195);
-    stroke('rgb(0,0,0)');
-    logoImage();
-    logoShape();
-    logoTypography();
+  background(250,150,195);
+  stroke('rgb(0,0,0)');
+  logoImage();
+  logoTypography();
+  
+  // Draw a circle
+  
+    lemonLogoHeight = 200;
+  
+    // Lemon Shape
+    strokeWeight(8);
+    fill(246, 229, 20);
+    arc(x, y, 200, lemonLogoHeight, HALF_PI, PI + HALF_PI, CHORD);
+    arc(x, y, 200, lemonLogoHeight, HALF_PI, PI + HALF_PI, CHORD);
+    fill(247, 235, 95);
+    arc(x, y, 150, lemonLogoHeight, HALF_PI, PI + HALF_PI);
+  
+  // Lemon Lines
+    strokeWeight(3);
+    
+    line(x, y, x - 60, y + 64);
+    line(x, y, x - 60, y - 64);
+    line(x, y, x - 75, y );
+ 
+  
+  // Jiggling randomly on the horizontal axis
+  x = x + random(-3, 3);
+  // Moving up at a constant speed
+  y = y - 1;
+  
+  // Reset to the bottom
+  if (y < 0) {
+    y = height;
+  }
 }
--- week6.js
+++ week6.js
@@ -1,3 +1,91 @@
 /*
     Paste the code for your week 3 exercise below.
 */
+let x, y;
+let img; 
+let r, g, b;
+
+// Writes the Logo Image
+function logoImage() {
+ 
+  square(50, 50, 400);
+  image(img, 50, 50, 400, 400);
+
+}
+function logoTypography() {
+
+    textWrap(CHAR);
+    textStyle(BOLDITALIC);
+    strokeWeight(0);
+    textSize(50);
+    fill(0,0,0);
+    text('LUCYS', 170, 120, );
+    text('LEMONS', 150, 410 );
+  }
+  function logoShape() {
+
+   
+    let lemonLogoHeight;
+
+    lemonLogoHeight = 200;
+  
+    // Lemon Shape
+    stroke(r, g, b);
+  fill(r, g, b, 127);
+    arc(x, y, 200, lemonLogoHeight, HALF_PI, PI + HALF_PI, CHORD);
+    arc(x, y, 200, lemonLogoHeight, HALF_PI, PI + HALF_PI, CHORD);
+    fill(r, g, b, 127);
+    arc(x, y, 150, lemonLogoHeight, HALF_PI, PI + HALF_PI);
+  
+  // Lemon Lines
+    strokeWeight(3);
+    stroke(255);
+    line(x, y, x - 60, y + 64);
+    line(x, y, x - 60, y - 64);
+    line(x, y, x - 75, y );
+ 
+    
+  }
+  
+
+function setup() {
+  createCanvas(500, 500);
+  img = loadImage('assets/backgroundlemon.jpg');
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  // Starts in the middle
+  x = width / 2;
+  y = height;
+
+}
+
+function draw() {
+  background(250,150,195);
+  stroke('rgb(0,0,0)');
+  logoImage();
+  logoTypography();
+  logoShape();
+  
+  
+  
+  // Draw a circle
+  
+  
+  // Jiggling randomly on the horizontal axis
+  x = 300 + random(2, 2);
+  // Moving up at a constant speed
+  y = y - 2;
+  
+  // Reset to the bottom
+  if (y < 0) {
+    y = height;
+  }
+
+}
+function mouseClicked() {
+   
+      r = random(255);
+      g = random(255);
+      b = random(255);
+}
